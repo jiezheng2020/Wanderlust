551226187695c524ba42fcd8d79409cf8aebc2e6
Kevin Zheng <jiezheng2020@gmail.com>
2021-04-27 17:50:54 -0500

    started on splash page

--- authenticate-me/frontend/src/components/HomePage/index.js
+++ authenticate-me/frontend/src/components/HomePage/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import "./HomePage.css";
 
 export default function HomePage() {
@@ -9,5 +9,40 @@ export default function HomePage() {
 
   if (sessionUser) return <Redirect to="/authorized"></Redirect>;
 
-  return <div className="homepage">This is HomePage</div>;
+  return (
+    <div className="homepage">
+      <div className="hero-banner">
+        <div className="hero-text">
+          <h1>Welcome to Wanderlust,</h1>
+          <h1>Sign up today to access</h1>
+          <h1>our features!</h1>
+
+          <div className="h3-div">
+            <h3>Join a group to meet new people,</h3>
+            <h3>find a community, and travel together!</h3>
+          </div>
+        </div>
+        <div className="image-container">
+          <div className="hero-image" />
+        </div>
+      </div>
+      <div className="home-links">
+        <div className="link-containers">
+          <div className="event-container">
+            <div className="event-links"></div>
+            <Link to="/">Make new friends</Link>
+          </div>
+          <div className="event-container">
+            <div className="event-links"></div>
+            <Link to="/">Explore the outdoors</Link>
+          </div>
+          <div className="event-container">
+            <div className="event-links"></div>
+            <Link to="/">Explore the nightlife</Link>
+          </div>
+        </div>
+      </div>
+      <div className="home-intro">This is Home Wanderlust works</div>
+    </div>
+  );
 }

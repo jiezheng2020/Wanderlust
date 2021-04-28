d9dd7e6dc44d9c7cb4075b2a3405dc6f2ce06d20
Kevin Zheng <jiezheng2020@gmail.com>
2021-04-27 23:54:25 -0500

    added comments faker data

---
+++ authenticate-me/backend/db/seeders/20210428044106-add-comments.js
@@ -0,0 +1,36 @@
+"use strict";
+const faker = require("faker");
+const models = require("../models");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const CommentsData = [];
+
+    const events = await models.Event.findAll();
+    const user = await models.User.findAll();
+    const bodyArr = [
+      "This group has good events! Would recommend!",
+      "Sounds fun",
+      "Excited",
+      faker.commerce.productAdjective(),
+      faker.hacker.phrase(),
+    ];
+    for (let i = 0; i < 1000; i++) {
+      const randBody = Math.floor(Math.random() * bodyArr.length);
+      const randEvent = Math.floor(Math.random() * events.length + 1);
+      const randUser = Math.floor(Math.random() * user.length + 1);
+
+      let newComment = {
+        eventId: randEvent,
+        userId: randUser,
+        body: bodyArr[randBody],
+      };
+      CommentsData.push(newComment);
+    }
+    return queryInterface.bulkInsert("Comments", CommentsData, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Comments", null, {});
+  },
+};

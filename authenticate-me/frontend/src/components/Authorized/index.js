c09608fa8ac9a0af1c1fcc05d4a77bd837522e53
Kevin Zheng <jiezheng2020@gmail.com>
2021-04-28 11:53:44 -0500

    started css on auth

--- authenticate-me/frontend/src/components/Authorized/index.js
+++ authenticate-me/frontend/src/components/Authorized/index.js
@@ -9,5 +9,37 @@ export default function Authorized() {
 
   if (!sessionUser) return <Redirect to="/"></Redirect>;
 
-  return <div className="authorized-home">Authorized Home Page</div>;
+  return (
+    <div className="authorized-home">
+      <div className="events-containers">
+        <div classname="events-content">
+          <h1>Most Popular Events</h1>
+          <div className="events-row">
+            <div className="event-item">1</div>
+            <div className="event-item">2</div>
+            <div className="event-item">3</div>
+            <div className="event-item">4</div>
+          </div>
+        </div>
+        <div classname="events-content">
+          <h1>Events By</h1>
+          <div className="events-row">
+            <div className="event-item">1</div>
+            <div className="event-item">2</div>
+            <div className="event-item">3</div>
+            <div className="event-item">4</div>
+          </div>
+        </div>
+        <div classname="events-content">
+          <h1>Other Events</h1>
+          <div className="events-row">
+            <div className="event-item">1</div>
+            <div className="event-item">2</div>
+            <div className="event-item">3</div>
+            <div className="event-item">4</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

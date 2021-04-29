f764ac08cce75e6a828590f6d3be370fa4014901
Kevin Zheng <jiezheng2020@gmail.com>
2021-04-29 14:54:01 -0500

    css for specific event

--- authenticate-me/frontend/src/components/Event/index.js
+++ authenticate-me/frontend/src/components/Event/index.js
@@ -22,8 +22,18 @@ export default function Event() {
 
   return (
     <div className="event-page">
-      <div className="event-header">Event Name, Time, Hosted by</div>
-      <div className="event-content">Event Image, Event Group, Event Time</div>
+      <div className="event-header">
+        <h4>Thursday, April 29, 2021</h4>
+        <h2>The Player(1992)</h2>
+        <h4>Hosted By Mindy</h4>
+      </div>
+      <div className="event-content">
+        <img className="event-image" src={event.image} />
+        <div className="event-content-text">
+          <label className="event-content-group">Event Group</label>
+          <label className="event-content-time">Event Time</label>
+        </div>
+      </div>
       <div className="event-details">Event Details</div>
       <div className="event-attendees">Event Attendees</div>
       <div className="event-comments">Event Comments</div>
